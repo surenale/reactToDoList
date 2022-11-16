f22cb02a861ed424a82c1fd340c671a774c5bcb0
surenGit <surenale69@gmail.com>
2022-11-16 23:23:39 +0545

    initializing empty array using hooks and storing the items in array once onClick on add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
 import React, { useState } from 'react';
+import ToDoList from './ToDoList';
 
 const App = () => {
 
     // useState hooks for handling the state of the input field
-    const [inputList, setInputList] = useState("buy apple");
+    const [inputList, setInputList] = useState("");
+
+    // creating empty array for storing the list using hooks
+    const [items, setItems] = useState([]);
 
     const itemEvent = (event) => {
+        // updating the input field
         setInputList(event.target.value);
+    };
+
+
+    // while onClicking the button 
+    const listOfItems = () => {
+        // concatenating the old item in array and getting new item in array
+        setItems((oldItems) => {
+            return [...oldItems, inputList];
+        });
+        // clearing the input field just clicking the add button
+        setInputList("");
+
     }
+
+
     return (
         <>
             <div className='main_div'>
                 <div className='center_div'>
                     <br />
                     <h1>ToDo List</h1>
-                    <input type="text" placeholder='Add a Items' onChange={itemEvent} />
-                    <button> + </button>
+                    <input type="text" placeholder='Add a Items' onChange={itemEvent}
+                        value={inputList} />
+                    <button onClick={listOfItems}> + </button>
                     <ol>
-                        <li>{inputList}</li>
+                        {/* <li>{inputList}</li> */}
+                        {
+                            // map method is used for accesing the items of array 
+                            items.map((itemVal) => {
+                                return <ToDoList
+                                    text={itemVal}
+                                />
+                            })
+                        }
                     </ol>
                 </div>
             </div>
---
+++ src/ToDoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ToDoList = (props) => {
+    return <li>{props.text}</li>
+}
+
+export default ToDoList;
