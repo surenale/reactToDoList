cb90d859cba21ebb3db0e92f2d52231e956935e7
surenGit <surenale69@gmail.com>
2022-11-16 22:45:22 +0545

    handling input text field with target value and show them in list

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
+    // useState hooks for handling the state of the input field
+    const [inputList, setInputList] = useState("buy apple");
+
+    const itemEvent = (event) => {
+        setInputList(event.target.value);
+    }
     return (
         <>
-        <div className='main_div'>
-            <div className='center_div'>
-                <br />
-                <h1>ToDo List</h1>
-                <input type="text" placeholder='Add a Items' />
-                <button> + </button>
-                <ol>
-                    <li>buy apple</li>
-                </ol>
+            <div className='main_div'>
+                <div className='center_div'>
+                    <br />
+                    <h1>ToDo List</h1>
+                    <input type="text" placeholder='Add a Items' onChange={itemEvent} />
+                    <button> + </button>
+                    <ol>
+                        <li>{inputList}</li>
+                    </ol>
+                </div>
             </div>
-        </div>
         </>
     );
 };
