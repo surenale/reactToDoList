48b0e0044d6808d5c60b70d3ec679137d7372e42
surenGit <surenale69@gmail.com>
2022-11-16 20:03:50 +0545

    html and css part of ToDoList

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+        <div className='main_div'>
+            <div className='center_div'>
+                <br />
+                <h1>ToDo List</h1>
+                <input type="text" placeholder='Add a Items' />
+                <button> + </button>
+                <ol>
+                    <li>buy apple</li>
+                </ol>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default App;
